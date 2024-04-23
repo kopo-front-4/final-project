169e3aa42103b880d5b62ae529e2949b4b8ca468
mrbonk97 <mrbonk97@gmail.com>
2024-04-23 19:26:39 +0900

    Feat: add color

---
+++ constants.js
@@ -0,0 +1,41 @@
+const daily = [
+  {
+    message: "총 운 세메시지",
+  },
+];
+
+const fortune1 = [
+  {
+    score: 0,
+    message1: "asdasd",
+    message: "오늘의 운세는",
+  },
+];
+
+const fortune2 = [
+  {
+    score: 0,
+    message: "오늘의 운세는",
+  },
+];
+
+const fortune3 = [
+  {
+    score: 0,
+    message: "오늘의 운세는",
+  },
+];
+
+const fortune4 = [
+  {
+    score: 0,
+    message: "오늘의 운세는",
+  },
+];
+
+const fortune5 = [
+  {
+    score: 0,
+    message: "오늘의 운세는",
+  },
+];
